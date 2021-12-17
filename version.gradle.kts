386d39334c968122db61af8547044512000e2996
Alex Tymchenko <alex.tymchenko@teamdev.com>
2021-12-17 13:27:24 +0200

    Bump the version of this library and its dependencies.

--- version.gradle.kts
+++ version.gradle.kts
@@ -25,6 +25,6 @@
  */
 
 
-val spineCoreVersion by extra("1.7.0")
-val spineBaseVersion by extra("1.7.0")
-val versionToPublish by extra("1.7.1-SNAPSHOT.2")
+val spineCoreVersion by extra("1.8.0")
+val spineBaseVersion by extra("1.8.0")
+val versionToPublish by extra("1.8.0")
