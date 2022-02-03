93ebbbf7c6695ffaaaf453fb6be45cbb70c7792c
Alex Tymchenko <alex.tymchenko@teamdev.com>
2022-02-03 17:16:11 +0200

    Optimize imports.

--- rdbms/src/test/java/io/spine/server/storage/jdbc/delivery/JdbcShardedWorkRegistryTest.java
+++ rdbms/src/test/java/io/spine/server/storage/jdbc/delivery/JdbcShardedWorkRegistryTest.java
@@ -37,9 +37,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static com.google.common.truth.Truth.assertThat;
-import static com.google.common.truth.Truth8.assertThat;
-import static com.google.common.util.concurrent.Uninterruptibles.sleepUninterruptibly;
 import static io.spine.server.ContextSpec.singleTenant;
 import static io.spine.server.storage.jdbc.delivery.given.TestShardIndex.newIndex;
 import static io.spine.server.storage.jdbc.given.JdbcStorageFactoryTestEnv.newFactory;
