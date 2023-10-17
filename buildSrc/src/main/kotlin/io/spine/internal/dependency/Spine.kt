a977b318bf10f0e7281fde06ba5da6a34708dabc
Alex Tymchenko <alex.tymchenko@teamdev.com>
2023-10-17 17:45:33 +0100

    Use the latest version of Spine core libraries.

--- buildSrc/src/main/kotlin/io/spine/internal/dependency/Spine.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/dependency/Spine.kt
@@ -75,7 +75,7 @@ object Spine {
          * @see [Spine.CoreJava.server]
          * @see <a href="https://github.com/SpineEventEngine/core-java">core-java</a>
          */
-        const val core = "2.0.0-SNAPSHOT.159"
+        const val core = "2.0.0-SNAPSHOT.161"
 
         /**
          * The version of [Spine.modelCompiler].
