4a1d0a5a26724ca8e3a36e3e6edf80bc33aab461
Alex Tymchenko <alex.tymchenko@teamdev.com>
2023-12-03 17:07:09 +0000

    Move the test-only API to the respective test environment.

--- rdbms/src/test/java/io/spine/server/storage/jdbc/operation/OperationFactoryTest.java
+++ rdbms/src/test/java/io/spine/server/storage/jdbc/operation/OperationFactoryTest.java
@@ -26,44 +26,23 @@
 
 package io.spine.server.storage.jdbc.operation;
 
-import com.google.protobuf.Message;
-import io.spine.base.Identifier;
-import io.spine.client.EntityId;
-import io.spine.core.Command;
-import io.spine.server.delivery.DeliveryStrategy;
-import io.spine.server.delivery.InboxId;
-import io.spine.server.delivery.InboxLabel;
 import io.spine.server.delivery.InboxMessage;
 import io.spine.server.delivery.InboxMessageId;
-import io.spine.server.delivery.InboxMessageMixin;
-import io.spine.server.delivery.InboxSignalId;
 import io.spine.server.storage.Storage;
-import io.spine.server.storage.jdbc.DataSourceWrapper;
-import io.spine.server.storage.jdbc.JdbcStorageFactory;
-import io.spine.server.storage.jdbc.TypeMapping;
-import io.spine.server.storage.jdbc.query.InsertOneQuery;
-import io.spine.server.storage.jdbc.record.JdbcRecord;
-import io.spine.server.storage.jdbc.record.RecordTable;
-import io.spine.test.storage.StgProject;
-import io.spine.type.TypeUrl;
+import io.spine.server.storage.jdbc.operation.given.OperationFactoryTestEnv.TestOperationFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth8.assertThat;
-import static io.spine.base.Identifier.newUuid;
-import static io.spine.base.Time.currentTime;
-import static io.spine.server.delivery.InboxLabel.CATCH_UP;
-import static io.spine.server.delivery.InboxLabel.HANDLE_COMMAND;
-import static io.spine.server.delivery.InboxMessageStatus.DELIVERED;
-import static io.spine.server.storage.jdbc.GivenDataSource.whichIsStoredInMemory;
 import static io.spine.server.storage.jdbc.given.JdbcStorageFactoryTestEnv.deliveryContextSpec;
 import static io.spine.server.storage.jdbc.given.JdbcStorageFactoryTestEnv.inboxMessageSpec;
+import static io.spine.server.storage.jdbc.operation.given.OperationFactoryTestEnv.OVERRIDDEN_LABEL;
+import static io.spine.server.storage.jdbc.operation.given.OperationFactoryTestEnv.imStorageFactoryBuilder;
+import static io.spine.server.storage.jdbc.operation.given.OperationFactoryTestEnv.randomHandleCommandMessage;
 
 @DisplayName("With `OperationFactory` it should be possible")
 final class OperationFactoryTest {
-
-    private static final InboxLabel OVERRIDDEN_LABEL = CATCH_UP;
 
     @Test
     @DisplayName("use custom operation instead of a default one")
@@ -75,94 +54,15 @@ final class OperationFactoryTest {
                 factory.createRecordStorage(deliveryContextSpec(), inboxMessageSpec());
 
         var message = randomHandleCommandMessage();
+        assertThat(message.getLabel())
+                .isNotEqualTo(OVERRIDDEN_LABEL);
         storage.write(message.getId(), message);
+
         var actual = storage.read(message.getId());
         assertThat(actual)
                 .isPresent();
-        assertThat(actual.get().getLabel())
+        assertThat(actual.get()
+                         .getLabel())
                 .isEqualTo(OVERRIDDEN_LABEL);
-    }
-
-    private static JdbcStorageFactory.Builder imStorageFactoryBuilder() {
-        return JdbcStorageFactory
-                .newBuilder()
-                .setDataSource(whichIsStoredInMemory(newUuid()));
-    }
-
-    private static class TestOperationFactory extends OperationFactory {
-
-        private TestOperationFactory(DataSourceWrapper wrapper, TypeMapping mapping) {
-            super(wrapper, mapping);
-        }
-
-        @Override
-        public <I, R extends Message> WriteOne<I, R> writeOne(RecordTable<I, R> table) {
-            return new InboxWriteOne<>(table, dataSource());
-        }
-    }
-
-    private static InboxMessage randomHandleCommandMessage() {
-        var index = DeliveryStrategy.newIndex(0, 1);
-        var id = InboxMessageMixin.generateIdWith(index);
-        var signalId = InboxSignalId.newBuilder()
-                .setValue("some-command-id");
-        var result = InboxMessage.newBuilder()
-                .setId(id)
-                .setStatus(DELIVERED)
-                .setCommand(Command.getDefaultInstance())
-                .setInboxId(randomInboxId())
-                .setSignalId(signalId)
-                .setLabel(HANDLE_COMMAND)
-                .setWhenReceived(currentTime())
-                .setVersion(42)
-                .build();
-        return result;
-    }
-
-    private static InboxId randomInboxId() {
-        var packedId = Identifier.pack(newUuid());
-        return InboxId.newBuilder()
-                .setEntityId(EntityId.newBuilder()
-                                     .setId(packedId)
-                                     .build())
-                .setTypeUrl(TypeUrl.of(StgProject.class).value())
-                .build();
-    }
-
-    private static class InboxWriteOne<I, R extends Message> extends WriteOne<I, R> {
-
-        InboxWriteOne(RecordTable<I, R> table, DataSourceWrapper dataSource) {
-            super(table, dataSource);
-        }
-
-        @Override
-        public void execute(JdbcRecord<I, R> record) {
-            if(record.original().record() instanceof InboxMessage) {
-                modifyAndWrite(record);
-            } else {
-                super.execute(record);
-            }
-        }
-
-        @SuppressWarnings("unchecked")
-        private void modifyAndWrite(JdbcRecord<I, R> record) {
-            var castRecord = (JdbcRecord<InboxMessageId, InboxMessage>) record;
-            var originalMessage = castRecord.original()
-                                            .record();
-            var modifiedMessage = originalMessage
-                    .toBuilder()
-                    .setLabel(OVERRIDDEN_LABEL)
-                    .build();
-            var modifiedRecord = castRecord.copyWithRecord(
-                    modifiedMessage);
-            var castTable = (RecordTable<InboxMessageId, InboxMessage>) table();
-            var query = InsertOneQuery
-                    .<InboxMessageId, InboxMessage>newBuilder()
-                    .setTableSpec(castTable.spec())
-                    .setDataSource(dataSource())
-                    .setRecord(modifiedRecord)
-                    .build();
-            query.execute();
-        }
     }
 }
