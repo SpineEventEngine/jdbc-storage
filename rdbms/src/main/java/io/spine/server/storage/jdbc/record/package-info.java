f9138d5cef1de1d1853591d2cbd10e82041faadb
Alex Tymchenko <alex.tymchenko@teamdev.com>
2023-12-22 17:43:52 +0000

    Mark package as `@SPI`.

--- rdbms/src/main/java/io/spine/server/storage/jdbc/record/package-info.java
+++ rdbms/src/main/java/io/spine/server/storage/jdbc/record/package-info.java
@@ -28,10 +28,12 @@
  * This package provides types and routines for accessing stored {@link com.google.protobuf.Message
  * Message} records.
  */
+@SPI
 @CheckReturnValue
 @ParametersAreNonnullByDefault
 package io.spine.server.storage.jdbc.record;
 
 import com.google.errorprone.annotations.CheckReturnValue;
+import io.spine.annotation.SPI;
 
 import javax.annotation.ParametersAreNonnullByDefault;
