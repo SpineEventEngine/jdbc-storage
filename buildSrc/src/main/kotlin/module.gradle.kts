45b5957753c76901aad4e82d74bddb6f5a3b2bfe
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2026-06-19 20:32:07 +0100

    Improve build scripts

--- buildSrc/src/main/kotlin/module.gradle.kts
+++ buildSrc/src/main/kotlin/module.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025, TeamDev. All rights reserved.
+ * Copyright 2026, TeamDev. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,8 +24,206 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-// This is a template file for an actual script which should be
-// defined by a project to which `config` is applied.
-//
-// The reason for having this file is that it is referenced as
-// a plugin in `uber-jar-module.gradle.kts`
+import io.spine.dependency.boms.BomsPlugin
+import io.spine.dependency.build.ErrorProne
+import io.spine.dependency.lib.Grpc
+import io.spine.dependency.lib.Guava
+import io.spine.dependency.lib.Jackson
+import io.spine.dependency.lib.Kotlin
+import io.spine.dependency.lib.KotlinPoet
+import io.spine.dependency.local.Base
+import io.spine.dependency.local.BaseTypes
+import io.spine.dependency.local.Change
+import io.spine.dependency.local.Compiler
+import io.spine.dependency.local.CoreJvm
+import io.spine.dependency.local.Logging
+import io.spine.dependency.local.Reflect
+import io.spine.dependency.local.TestLib
+import io.spine.dependency.local.Time
+import io.spine.dependency.local.ToolBase
+import io.spine.dependency.local.Validation
+import io.spine.dependency.test.Jacoco
+import io.spine.dependency.test.JUnit
+import io.spine.gradle.checkstyle.CheckStyleConfig
+import io.spine.gradle.github.pages.updateGitHubPages
+import io.spine.gradle.javac.configureErrorProne
+import io.spine.gradle.javac.configureJavac
+import io.spine.gradle.javadoc.JavadocConfig
+import io.spine.gradle.kotlin.setFreeCompilerArgs
+import io.spine.gradle.publish.IncrementGuard
+import io.spine.gradle.repo.standardToSpineSdk
+import io.spine.gradle.report.license.LicenseReporter
+import org.gradle.jvm.tasks.Jar
+
+plugins {
+    `java-library`
+    id("org.jetbrains.kotlinx.kover")
+    id("com.google.protobuf")
+    id("net.ltgt.errorprone")
+    kotlin("jvm")
+    pmd
+    `maven-publish`
+    id("pmd-settings")
+    id("dokka-setup")
+    id("module-testing")
+    id("detekt-code-analysis")
+    idea
+}
+apply<BomsPlugin>()
+apply<IncrementGuard>()
+
+repositories.standardToSpineSdk()
+
+LicenseReporter.generateReportIn(project)
+JavadocConfig.applyTo(project)
+CheckStyleConfig.applyTo(project)
+
+project.run {
+    configureJava(BuildSettings.javaVersion)
+    configureKotlin()
+    addDependencies()
+    forceConfigurations()
+
+    setupPublishing()
+    configureTaskDependencies()
+}
+
+kover {
+    useJacoco(version = Jacoco.version)
+    reports.total.xml.onCheck = true
+}
+
+typealias Module = Project
+
+/**
+ * Configures Java tasks in this project.
+ */
+fun Module.configureJava(javaVersion: JavaLanguageVersion) {
+    java {
+        toolchain.languageVersion.set(javaVersion)
+    }
+    tasks {
+        withType<JavaCompile>().configureEach {
+            configureJavac()
+            configureErrorProne()
+        }
+        withType<Jar>().configureEach {
+            duplicatesStrategy = DuplicatesStrategy.INCLUDE
+        }
+    }
+}
+
+/**
+ * Configures Kotlin tasks in this project.
+ */
+fun Module.configureKotlin() {
+    kotlin {
+        explicitApi()
+        compilerOptions {
+            jvmTarget.set(BuildSettings.jvmTarget)
+            setFreeCompilerArgs()
+        }
+    }
+}
+
+/**
+ * Defines dependencies of this subproject.
+ */
+fun Module.addDependencies() {
+    dependencies {
+        ErrorProne.apply {
+            errorprone(core)
+        }
+        implementation(Validation.runtime)
+    }
+}
+
+/**
+ * Forces dependencies of this project.
+ */
+fun Module.forceConfigurations() {
+    configurations {
+        forceVersions()
+        excludeProtobufLite()
+
+        all {
+            exclude(group = "io.spine", module = "spine-flogger-api")
+            exclude(group = "io.spine", module = "spine-logging-backend")
+            exclude(group = "io.spine", module = "spine-validate")
+
+            resolutionStrategy {
+                /* Force the version of gRPC used by the `:client` module over the one
+                   set by `mc-java` in the `:core` module when specifying compiler artifact
+                   for the gRPC plugin.
+                   See `io.spine.tools.mc.java.gradle.plugins.JavaProtocConfigurationPlugin
+                   .configureProtocPlugins()` method that sets the version from resources. */
+                Grpc.forceArtifacts(project, this@all, this@resolutionStrategy)
+                force(Grpc.ProtocPlugin.artifact)
+
+                // Substitute the legacy artifact coordinates with the new `ToolBase.lib` alias.
+                dependencySubstitution {
+                    substitute(module("io.spine.tools:spine-tool-base")).using(module(ToolBase.lib))
+                }
+
+                Jackson.forceArtifacts(project, this@all, this@resolutionStrategy)
+                Jackson.DataFormat.forceArtifacts(project, this@all, this@resolutionStrategy)
+                Jackson.DataType.forceArtifacts(project, this@all, this@resolutionStrategy)
+
+                force(
+                    Base.annotations,
+                    Base.environment,
+                    Base.format,
+                    Base.lib,
+                    BaseTypes.lib,
+                    Change.lib,
+                    Compiler.api,
+                    Compiler.backend,
+                    Compiler.pluginLib,
+                    Compiler.gradleApi,
+                    Compiler.params,
+                    Compiler.jvm,
+                    CoreJvm.server,
+                    Grpc.bom,
+                    Guava.lib,
+                    JUnit.bom,
+                    Jackson.annotations,
+                    Jackson.bom,
+                    Kotlin.bom,
+                    KotlinPoet.lib,
+                    Logging.grpcContext,
+                    Logging.lib,
+                    Logging.libJvm,
+                    Reflect.lib,
+                    TestLib.lib,
+                    Time.lib,
+                    Time.javaExtensions,
+                    ToolBase.gradlePluginApi,
+                    ToolBase.intellijPlatform,
+                    ToolBase.intellijPlatformJava,
+                    ToolBase.jvmTools,
+                    ToolBase.lib,
+                    ToolBase.pluginBase,
+                    ToolBase.protobufSetupPlugins,
+                    ToolBase.psiJava,
+                    Validation.context,
+                    Validation.javaBundle,
+                    Validation.gradlePluginLib,
+                    Validation.runtime,
+                )
+            }
+        }
+    }
+}
+
+/**
+ * Configures publishing for this subproject.
+ */
+fun Module.setupPublishing() {
+    updateGitHubPages {
+        rootFolder.set(rootDir)
+    }
+
+    tasks.named("publish") {
+        dependsOn("${project.path}:updateGitHubPages")
+    }
+}
